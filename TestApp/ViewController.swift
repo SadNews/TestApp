f2fcbf6208865191ae05faa1de0555abfe4812b3
Andrey Ushakov <sadnews@ya.ru>
2020-07-30 23:13:32 +0300

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Андрей Ушаков on 30.07.2020.
+//  Copyright © 2020 Андрей Ушаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
